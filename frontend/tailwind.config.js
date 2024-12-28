dd32cd66e49da755f2a6c33fb9938183356e9dc4
Nikhil6524 <132472349+Nikhil6524@users.noreply.github.com>
2024-12-28 21:50:48 +0530

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+import daisyui from "daisyui";
+import daisyUIThemes from "daisyui/src/theming/themes";
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [daisyui],
+
+	daisyui: {
+		themes: [
+			"light",
+			{
+				black: {
+					...daisyUIThemes["black"],
+					primary: "rgb(128, 0, 128)",
+					secondary: "rgb(25, 26, 25)",
+				},
+			},
+		],
+	},
+};
